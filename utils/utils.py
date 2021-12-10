683186bbd883588ed0ba959c08e37614a8c2662d
Miraz Zaidi <miraz.zaidi@klarna.com>
2021-12-10 16:56:21 +0100

    feat(util): Added utils.py.

---
+++ utils/utils.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def get_lines_from_file(filename: str) -> List[str]:
+    lines = []
+    with open(filename) as f:
+        lines = f.readlines()
+        lines = [line.rstrip() for line in lines]
+    return lines
