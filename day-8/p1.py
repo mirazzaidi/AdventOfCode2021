85b4b1aae4c0069390cdebcee957d15f9ba87221
Miraz Zaidi <miraz.zaidi@klarna.com>
2021-12-11 23:16:53 +0100

    feat(day-8): python code

---
+++ day-8/p1.py
@@ -0,0 +1,9 @@
+from utils.utils import get_lines_from_file
+
+if __name__ == "__main__":
+    lines = get_lines_from_file('day-8/input')
+
+    # strip the part before |
+    lines = [ line.split('|')[1].lstrip() for line in lines]
+    print("Total words: ", len([word for line in lines for word in line.split() if len(word) in [2,3,4,7]]))
+    
---
+++ day-8/p2.py
@@ -0,0 +1,82 @@
+from typing import List
+from utils.utils import get_lines_from_file
+
+def sorted_str(s):
+    return ''.join(sorted(s))
+
+def remove_overlapping_chars(s1, s2):
+    s1 = sorted_str(s1)
+    s2 = sorted_str(s2)
+    return sorted_str(''.join ([ch for ch in s1 if ch not in s2] + [ch for ch in s2 if ch not in s1]))
+
+def check(s1, s2):
+    for ch in s1:
+        if ch not in s2:
+            return False
+    return True
+
+if __name__ == "__main__":
+    lines = get_lines_from_file('day-8/input')
+
+    lines_left = [line.split('|')[0].rstrip() for line in lines]
+    lines_right = [line.split('|')[1].lstrip() for line in lines]
+    sum = 0
+    # // process 1,4,7,8
+    for i, line in enumerate(lines):
+
+        left, right = line.split('|')
+        left.rstrip()
+        right.lstrip()
+
+        words_on_left = [sorted_str(w) for w in left.split()]
+        words_on_right = [sorted_str(w) for w in right.split()]
+
+        one = ''
+        four = ''
+        seven = ''
+        wires = dict()
+
+        for word in words_on_left:
+            if len(word) == 2:
+                wires[word] = 1
+                one = word
+            elif len(word) == 3:
+                wires[word] = 7
+                seven = word
+            elif len(word) == 4:
+                wires[word] = 4
+                four = word
+            elif len(word) == 7:
+                wires[word] = 8
+
+        # remove 1,4,7,8
+        words_on_left = [w for w in words_on_left if len(w) not in [2,3,4,7]]
+
+        for word in words_on_left:
+            word = sorted_str(word)
+            sz = len(word)
+
+            #  now we have a word for one of 0,2,3,5,6,or 9
+            if sz == 6: # Could be any of (0,6,9)
+                # Check for zero first.
+                if check(seven, word) and check(one, word) and not check(four, word):
+                    wires[word] = 0
+                # now check for 9
+                elif check(seven, word) and check(one, word) and check(four, word):
+                    wires[word] = 9
+                else:
+                    wires[word] = 6
+            elif sz == 5: # 2, 3, 5
+                # Check for 3
+                if check(seven, word): # and check(one, word):
+                    wires[word] = 3
+                elif check(remove_overlapping_chars(four, seven), word):
+                    wires[word] = 5
+                else:
+                    wires[word] = 2
+
+        num = int(''.join ([str(wires[w]) for w in words_on_right]))
+        sum += num
+    
+    print("Ans is ", sum)
+    
