9d17359e0c5a1c2926bc76084101982443180920
Miraz Zaidi <miraz.zaidi@klarna.com>
2021-12-11 23:28:06 +0100

    format(): autopep8

--- day-8/p1.py
+++ day-8/p1.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     lines = get_lines_from_file('day-8/input')
 
     # strip the part before |
-    lines = [ line.split('|')[1].lstrip() for line in lines]
-    print("Total words: ", len([word for line in lines for word in line.split() if len(word) in [2,3,4,7]]))
-    
+    lines = [line.split('|')[1].lstrip() for line in lines]
+    print("Total words: ", len(
+        [word for line in lines for word in line.split() if len(word) in [2, 3, 4, 7]]))
